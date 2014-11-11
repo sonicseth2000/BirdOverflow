3a5477759607e4d23880da70ad871e8ac2960816
fabetron <mattfaber11@yahoo.com>
2014-11-10 18:12:33 -0600

    valid3

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,11 @@
 class Question < ActiveRecord::Base
+	
 	validates :Question_ID, presence: true, numericality: true
 	validates :Creator_ID, presence: true, numericality: true
 	validates :Timestamp, presence: true
+	
+	belongs_to :user
+	
+	self.primary_key = :Question_ID
+
 end
