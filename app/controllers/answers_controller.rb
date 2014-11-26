0afa6c7a09d3b9d04218ba293a2a729dacc51949
Travis91 <tasaude@ilstu.edu>
2014-11-26 13:00:02 -0600

    minor questions bug fix

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
-   belongs_to :question
+  
   # GET /answers
   # GET /answers.json
   def index
