e8fe2955c0521ffe49d6d39184cb5a014249d4e4
fabetron <mattfaber11@yahoo.com>
2014-11-26 18:15:19 -0600

    makes content and title unique and filled in

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
 has_many :answers
   validates_uniqueness_of :content 
+  validates_uniqueness_of :title
+  validates_presence_of :content
+  validates_presence_of :title
 end
